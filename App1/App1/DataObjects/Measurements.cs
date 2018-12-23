7a2ffa8fefb6d0c389e6e23091f715dd433149c1
aviv.haliva <aviv.haliva@gmail.com>
2018-12-23 19:17:45 +0200

    Add Activities and Measurements Pages + DBs Access

---
+++ App1/App1/DataObjects/Measurements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.DataObjects
+{
+    class Measurements
+    {
+        public string Id { get; set; }
+        public string UserID { get; set; }
+        public DateTime Date { get; set; }
+        public int TRI {get; set; }
+        public double PNN50 { get; set; }
+        public double SDNN { get; set; }
+        public double SDSD { get; set; }
+        public string ActivityID { get; set; }
+        public int StressIndex { get; set; }
+        public int IsStressed { get; set; }
+        public double GPSLat { get; set; }
+        public double GPSLng { get; set; }
+
+    }
+}
--- App1/App1/StatsPageToc.xaml.cs
+++ App1/App1/StatsPageToc.xaml.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using App1.Pages;
 
 namespace App1
 {
@@ -20,6 +20,16 @@ namespace App1
         public void calanderClicked(object sender, EventArgs args) { }
 
         public void mapClicked(object sender, EventArgs args) { }
+
+        public async void activitiesClicked(object sender, EventArgs args) {
+            await Navigation.PushAsync(new ActivitiesListPage());
+        }
+
+        public async void lastMeasurementsClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new LastMeasurementsListPage());
+        }
         
+
     }
 }
