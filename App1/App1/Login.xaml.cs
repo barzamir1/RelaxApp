c9fe90f7253fb89fa9a7582c54fa9a7cb91ac21d
barzamir1 <39021031+barzamir1@users.noreply.github.com>
2018-12-28 13:40:39 +0200

    Revert "Revert "make Login the first page""
    This reverts commit bc6db1784e796d34e7d3b297ae56fc3c37354f47.

--- App1/App1/Login.xaml.cs
+++ App1/App1/Login.xaml.cs
@@ -2,9 +2,11 @@
 using Microsoft.WindowsAzure.MobileServices;
 using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
 using Microsoft.WindowsAzure.MobileServices.Sync;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,20 +30,27 @@ namespace App1
             Default = this;
             _mobileServiceClient = new MobileServiceClient("https://relaxapp.azurewebsites.net");
             const string path = "syncstore.db";
+
             //setup our local sqlite store and intialize our table
             var store = new MobileServiceSQLiteStore(path);
             store.DefineTable<Users>();
-
             ServiceClient.SyncContext.InitializeAsync(store, new MobileServiceSyncHandler());
 
             //Get our sync table that will call out to azure
             UsersTable = ServiceClient.GetSyncTable<Users>();
             UsersTable.PullAsync("Users", UsersTable.CreateQuery());
+
+            RetrieveUserFromLocalStorage();
         }
 
+        //class data members
         public static Login Default
         {
-            get { return defaultInstance; }
+            get
+            {
+                defaultInstance = defaultInstance ?? new Login();
+                return defaultInstance;
+            }
             private set { defaultInstance = value; }
         }
         public MobileServiceClient ServiceClient
@@ -51,8 +60,10 @@ namespace App1
         public Users CurrentUser
         {
             get { return currentUser; }
+            set { currentUser = value; }
         }
 
+        //functions
         async void loginButton_Clicked(object sender, EventArgs e)
         {
             if (App.Authenticator != null)
@@ -62,18 +73,46 @@ namespace App1
                 if (authenticated)
                 {
                     String userId = ServiceClient.CurrentUser.UserId.Substring(4);
-                    currentUser = await UsersTable.LookupAsync(userId);
-                    await Navigation.PushAsync(new Page1()); //navigate to home page
-                }
-                else
-                {
-                    //navigate to registration page
+                    currentUser = await UsersTable.LookupAsync(userId); //check if user exist in Users table
+                    if (currentUser != null)
+                    {
+                        Application.Current.Properties["user_id "] = userId; //save user id to local storage
+                        Application.Current.Properties["user_token "] = _mobileServiceClient.CurrentUser.MobileServiceAuthenticationToken;
+                        await Application.Current.SavePropertiesAsync();
+
+                        await Navigation.PushAsync(new Page1()); //navigate to home page
+                    }
+                    else
+                    {
+                        //navigate to registration page
+                    }
+
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }   
+        }
+       async void RetrieveUserFromLocalStorage()
+        {
+            try
+            {
+                //check if the user previously signed in
+                var currentuserID = (Application.Current.Properties["user_id "]); //get user id from local storage
+                var token = (Application.Current.Properties["user_token "]);
+                if (currentuserID != null)
+                {
+                    _mobileServiceClient.CurrentUser = new MobileServiceUser(currentuserID.ToString());
+                    _mobileServiceClient.CurrentUser.MobileServiceAuthenticationToken = token.ToString();
+                    CurrentUser = await UsersTable.LookupAsync(currentuserID.ToString());
+                    await Navigation.PushAsync(new Page1()); //navigate to home page
+                }
+            }
+            catch (Exception e)//no user id in local storage. user must press the Login button
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
